b1673fc1867a915ffdffa86f1646aa630a8b43a6
Christian <christian.siegrist@open-circle.ch>
2025-07-14 16:38:32 +0200

    added return if no saves found

--- main.rb
+++ main.rb
@@ -20,13 +20,16 @@ p "Wellcome to Hangman, insert 'new' for a fresh Game or 'load' to load a Game"
 input = gets.chomp
 if input.downcase == 'load'
   saves = Dir.glob('saves/*').map { |file| file[(file.index('/') + 1)...(file.index('.'))] }
+  if saves.length < 1
+    p "no saves found"
+    return
+  else
   p 'insert savename'
   puts saves
-  
   load_name = gets.chomp
   Round.play_round(false)
   LoadGame.load(load_name)
-
+  end
 elsif input.downcase == 'exit'
   return
 else
